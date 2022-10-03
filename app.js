b97a8516abe9f58e8876125dac004bb248fe3b40
devPC <bvsvenema@gmail.com>
2022-10-03 08:44:52 +0200

    update

--- app.js
+++ app.js
@@ -6,20 +6,21 @@ const app = express();
 // listen for requests
 app.listen(3000);
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', {root: __dirname});
+    res.render('index');
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', {root: __dirname});
-});
-
-// redericts
-app.get('/about-us', (req,res) => {
-    res.redirect('/about');
-});
-
-//404 page
-app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', {root: __dirname})
-});
+    res.render('about');
+  });
+  
+  app.get('/blogs/create', (req, res) => {
+    res.render('create');
+  });
+  
+  // 404 page
+  app.use((req, res) => {
+    res.status(404).render('404');
+  });
