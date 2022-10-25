295408e9a7358d924e4cdd7a65adb8140e02a9e3
devPC <bvsvenema@gmail.com>
2022-10-25 15:56:50 +0200

    123

--- router/apiAdmin.js
+++ router/apiAdmin.js
@@ -1,89 +1,81 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 router.use(express.urlencoded({ extended: true }));
 
-const sanitize = require('mongo-sanitize');
-const model = require('../API/models/user');
-const bcrypt = require('bcryptjs');
-const user = require('../API/models/user');
-const Api = require('./api')
-
+const sanitize = require("mongo-sanitize");
+const model = require("../API/models/user");
+const bcrypt = require("bcryptjs");
+const user = require("../API/models/user");
+const Api = require("./api");
 
 router.use(async (req, res, next) => {
-
-    if (!req.auth || req.admin <= 2) return res.status(401).render('login', { errMsg: "Unauthorized!" });
-    else next();
+  if (!req.auth || req.admin <= 2)
+    return res.status(401).render("login", { errMsg: "Unauthorized!" });
+  else next();
 });
 
-router.post('/user/:id', (req, res) =>{
-    const id = req.params.id;//get the id of the text
-    //find the model in the database and delete it
-    user.findByIdAndRemove(id, (err) =>{
-        if(err){
-            console.log(err)
-            return res.json({ success: false });
-        }
-        res.redirect('/admin');
-    })
 
-})
-
-
-router.route('/user/:action')
-    .post((req, res) => {
-        switch (req.params.action) {
-            case "register":
-                userRegister(req, res);
-                break;
-            case "delete":
-                userDelete(req,res)
-                break;
-            default:
-                res.sendStatus(419);
-                break;
-
-        }
-    });
+router.route("/user/:action").post((req, res) => {
+  switch (req.params.action) {
+    case "register":
+      userRegister(req, res);
+      break;
+    default:
+      res.sendStatus(419);
+      break;
+  }
+});
 
 
 module.exports = router;
-
+async function userDelete(req,res){
+  const id = req.params.id;//get the id of the text
+  //find the model in the database and delete it
+  user.findByIdAndRemove(id, (err) =>{
+      if(err){
+          console.log(err)
+          return res.json({ success: false });
+      }
+      res.redirect('/admin');
+  })
+}
 // Functions
 async function userRegister(req, res) {
-    //check if all things has been filled in
+  //check if all things has been filled in
 
 
-    const { userMail, password, firstName, lastName, admin } = sanitize(req.body);
-    if (!(userMail && password && firstName && lastName && !(isNaN(admin)))) {
-        console.log('Test 1')
-        return  user.find().sort({CreatedAt: -1 })
-        .then((result) =>{ res.status(400).render('admin', {user: result, admin: req.auth.admin, errMsg: "Incomplete request!" })
-    }).catch((err)=>{
-        console.log(err);
-    });
-    }
+  const { userMail, password, firstName, lastName, admin } = sanitize(req.body);
+  if (!(userMail && password && firstName && lastName && !(isNaN(admin)))) {
+      console.log('Test 1')
+      return  user.find().sort({CreatedAt: -1 })
+      .then((result) =>{ res.status(400).render('admin', {user: result, admin: req.auth.admin, errMsg: "Incomplete request!" })
+  }).catch((err)=>{
+      console.log(err);
+  });
+  }
 
-    //check if user already exist
-    const email = `${userMail}`;
-    const oldUser = await user.findOne({ email: email });
-    if (oldUser) return user.find().sort({CreatedAt: -1 })
-    .then((result) =>{ res.status(400).render('admin', {user: result, admin: req.auth.admin, errMsg: "User already exist" })
-    }).catch((err)=>{
-        console.log(err);
-    });
+  //check if 
+  //check if user already exist
+  const email = `${userMail}`;
+  const oldUser = await user.findOne({ email: email });
+  if (oldUser) return user.find().sort({CreatedAt: -1 })
+  .then((result) =>{ res.status(400).render('admin', {user: result, admin: req.auth.admin, errMsg: "User already exist" })
+  }).catch((err)=>{
+      console.log(err);
+  });
 
-    //create the model
-    const fullName = `${firstName} ${lastName}`;
-    const passHash = bcrypt.hashSync(password, 10);
-    model.create({
-        firstName, lastName, fullName,
-        admin, email, passHash
-    });
+  //create the model
+  const fullName = `${firstName} ${lastName}`;
+  const passHash = bcrypt.hashSync(password, 10);
+  model.create({
+      firstName, lastName, fullName,
+      admin, email, passHash
+  });
 
-    user.find().sort({CreatedAt: -1 })
-    .then((result) =>{
-        res.render('admin', {user: result, admin: req.auth.admin , errMsg: "User succesfully registered!" })
-    }).catch((err)=>{
-        console.log(err);
-    });
+  user.find().sort({CreatedAt: -1 })
+  .then((result) =>{
+      res.render('admin', {admin: result, user: req.auth, title: "Admin", errMsg: "User succesfully registered!" })
+  }).catch((err)=>{
+      console.log(err);
+  });
 }
--- router/public.js
+++ router/public.js
@@ -1,29 +1,47 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 router.use(express.urlencoded({ extended: true }));
-const User = require('../API/models/user');
+const User = require("../API/models/user");
 
-router.get('/admin', async (req, res) => {
-    if (!req.auth) return res.status(401).render('login', {title: 'login'})
-    // TODO: handle authentication (move to apiAuth)
-    //res.status(200).render('admin', {title: 'Admin'});
-    console.log(req.auth.admin);
-    if(req.auth.admin <= 1) return res.status(400).redirect('/')
-    User.find().sort({CreatedAt: -1 })
-    .then((result) =>{
-        res.render('admin', {admin: result, user: req.auth , title: 'All blogs'})
-    }).catch((err)=>{
-        console.log(err);
+router.get("/admin", async (req, res) => {
+  if (!req.auth) return res.status(401).render("login", { title: "login" });
+  // TODO: handle authentication (move to apiAuth)
+  //res.status(200).render('admin', {title: 'Admin'});
+  console.log(req.auth.admin);
+  if (req.auth.admin <= 1) return res.status(400).redirect("/");
+  User.find()
+    .sort({ CreatedAt: -1 })
+    .then((result) => {
+      res.render("admin", {
+        admin: result,
+        user: req.auth,
+        title: "Admin"
+      });
+    })
+    .catch((err) => {
+      console.log(err);
     });
 });
 
-router.get('/', async (req, res) => {
-    // Verify user is logged in using req.auth
-    if (!req.auth) return res.status(401).render('login', {title: 'login'});
-    console.log('test');
-    // TODO: render actual rekenweb page (user is logged in)
-     return res.status(200).render("index", {user: req.auth, title: 'Home'});
-});
+router.post('/admin/:id', (req, res) =>{
+  const id = req.params.id;//get the id of the text
+  //find the model in the database and delete it
+  User.findByIdAndRemove(id, (err) =>{
+      if(err){
+          console.log(err)
+          return res.json({ success: false });
+      }
+      res.redirect('/admin');
+  })
 
+})
+
+router.get("/", async (req, res) => {
+  // Verify user is logged in using req.auth
+  if (!req.auth) return res.status(401).render("login", { title: "login" });
+  console.log("test");
+  // TODO: render actual rekenweb page (user is logged in)
+  return res.status(200).render("index", { user: req.auth, title: "Home" });
+});
 
 module.exports = router;
