9d7e93e00895d1d5dd96f29e9c104bbb8fceaed2
bvsvenema <bvsvenema@gmail.com>
2022-11-11 14:35:23 +0100

    idk

--- API/database.js
+++ API/database.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 
 exports.connect = () => {
     const dbURI = 'mongodb+srv://Bvsvenema:bCyOWJX9siLU7lcs@website.lqtups4.mongodb.net/Test?retryWrites=true&w=majority' 
-    mongoose.connect(dbURI).then((result))
-    .catch((err) => console.log(err));
+    mongoose.connect(dbURI).then(() => {
+        console.log(`Connected to database on `);
+    }).catch((err) => {
+        console.log(`Failed to connect to database on`);
+        console.error(err);
+        process.exit(1);
+    });
 }
